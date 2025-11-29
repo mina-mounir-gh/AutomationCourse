49a58f85356ffaf12c8dddb9039aeb89925553a4
Mina Mounir <mina.mounir.gh@gmail.com>
2025-11-29 14:39:38 +0200

    case 5

--- src/main/java/page/SearchPage.java
+++ src/main/java/page/SearchPage.java
@@ -9,6 +9,7 @@ public class SearchPage extends BasePage{
     private By search = By.xpath("//*[@id='searchbox_input']");
     private By firstResult = By.xpath("//span[text()='WebDriver - Selenium']//ancestor::a");
     private By forthResult = By.xpath("//*[@id='r1-3']//span[contains(text(),'TestNG')]");
+    private By secondResult = By.xpath("//article[@id='r1-1']//span[contains(text(),'cucumber')]/ancestor::a");
     public SearchPage(WebDriver driver) {
         super(driver);
     }
@@ -26,5 +27,10 @@ public class SearchPage extends BasePage{
         setText(search,txt);
         keyPressEnter(search);
         Assert.assertTrue(getElement(forthResult).getText().contains(resultTxt), "Text does not match");
+    }
+    public void checkSecondResult(String txt, String url) {
+        setText(search,txt);
+        keyPressEnter(search);
+        Assert.assertEquals(getElement(secondResult).getAttribute("href"),url);
     }
 }
--- src/test/java/tests/TestCase.java
+++ src/test/java/tests/TestCase.java
@@ -9,25 +9,21 @@ public class TestCase extends BaseTest{
 
     @Test(alwaysRun = true, enabled = true)
     public void getTitle(){
-       // driver.get("https://duckduckgo.com/");
         Assert.assertEquals(driver.getTitle(), "Google", "Page title does not match!");
     }
     @Test(alwaysRun = true, enabled = true)
     public void checkLogo(){
-       // driver.get("https://duckduckgo.com/");
         HomePage homePage = new HomePage(driver);
         homePage.checkLogo();
     }
     @Test(alwaysRun = true, enabled = true)
     public void findFirstResult(){
-       // driver.get("https://duckduckgo.com/");
         SearchPage searchPage = new SearchPage(driver);
         searchPage.checkFirstResult("Selenium WebDriver","https://www.selenium.dev/documentation/webdriver/");
     }
     @Test(alwaysRun = true, enabled = true)
     public void findForthResult(){
-        // driver.get("https://duckduckgo.com/");
         SearchPage searchPage = new SearchPage(driver);
-        searchPage.checkForthResult("TestNG","TestNG Tutorial");
+        searchPage.checkSecondResult("Cucumber IO","https://www.linkedin.com");
     }
 }
