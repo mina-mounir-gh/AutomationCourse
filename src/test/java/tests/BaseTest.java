82c6c58554c21e94477be599d218edcb013efef4
Mina Mounir <mina.mounir.gh@gmail.com>
2025-11-29 12:58:22 +0200

    Adding Remote driver instead of webdriver adding (alwaysRun = true) to fix all Tests execution

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,26 +1,18 @@
 package tests;
 
-import org.openqa.selenium.PageLoadStrategy;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
-
-import java.io.IOException;
 import java.lang.reflect.Method;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
 public class BaseTest {
-    WebDriver driver;
+    RemoteWebDriver driver;
 
-    @BeforeMethod
+    @BeforeMethod(alwaysRun = true)
     public void startDriver(){
         driver = new ChromeDriver();
+        driver.get("https://duckduckgo.com/");
     }
-   @AfterMethod
+   @AfterMethod(alwaysRun = true)
     public void closeDriver(Method method){
         driver.quit();
     }
--- src/test/java/tests/TestCase.java
+++ src/test/java/tests/TestCase.java
@@ -1,28 +1,26 @@
 package tests;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import page.HomePage;
 import page.SearchPage;
 
-public class Test1 extends BaseTest{
+public class TestCase extends BaseTest{
 
-    @Test
+    @Test(alwaysRun = true)
     public void test1(){
-        driver.get("https://duckduckgo.com/");
+       // driver.get("https://duckduckgo.com/");
         Assert.assertEquals(driver.getTitle(), "Google", "Page title does not match!");
     }
-    @Test
+    @Test(alwaysRun = true)
     public void test2(){
-        driver.get("https://duckduckgo.com/");
+       // driver.get("https://duckduckgo.com/");
         HomePage homePage = new HomePage(driver);
         homePage.checkLogo();
     }
-    @Test
+    @Test(alwaysRun = true)
     public void test3(){
-        driver.get("https://duckduckgo.com/");
+       // driver.get("https://duckduckgo.com/");
         SearchPage searchPage = new SearchPage(driver);
         searchPage.checkFirstResult("Selenium WebDriver","https://www.selenium.dev/documentation/webdriver/");
     }
